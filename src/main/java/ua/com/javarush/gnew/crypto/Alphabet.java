a5e36558f2069f150584246ce4725a152b109ca4
Seryii Lahuta <lagutasa@gmail.com>
2024-08-27 00:59:29 +0300

    Added Enum Alphabet

---
+++ src/main/java/ua/com/javarush/gnew/crypto/Alphabet.java
@@ -0,0 +1,21 @@
+package ua.com.javarush.gnew.crypto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public enum Alphabet {
+    ABC(new ArrayList<>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+            'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+            'X', 'Y', 'Z')));
+
+    private final ArrayList<Character> charsAlphabet;
+
+    Alphabet(ArrayList<Character> charsAlphabet) {
+        this.charsAlphabet = charsAlphabet;
+    }
+
+    public ArrayList<Character> getAlphabet() {
+        return this.charsAlphabet;
+    }
+
+}
