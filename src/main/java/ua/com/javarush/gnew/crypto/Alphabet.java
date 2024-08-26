4a9bb0aeb0272c80a377fa93960108700652f761
Seryii Lahuta <lagutasa@gmail.com>
2024-08-27 01:17:03 +0300

    add to Enum Alphabet another element

--- src/main/java/ua/com/javarush/gnew/crypto/Alphabet.java
+++ src/main/java/ua/com/javarush/gnew/crypto/Alphabet.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public enum Alphabet {
-    ABC(new ArrayList<>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+    ABC (new ArrayList<>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
             'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-            'X', 'Y', 'Z')));
+            'X', 'Y', 'Z'))),
+    PUNCTUATION (new ArrayList<>(Arrays.asList('.', ',', '!', '?', ':', ';', '-',
+            '(', ')', '[', ']', '{', '}','#', ' ', '@', '*', '+', '=', '_', '&',
+            '\\', '/', '₴', '~', '`')));
 
     private final ArrayList<Character> charsAlphabet;
 
