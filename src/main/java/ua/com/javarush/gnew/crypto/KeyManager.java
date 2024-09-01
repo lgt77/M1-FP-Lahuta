5159e5f4fb59143e2db0a8e9fc978bf3f8e534e4
Seryii Lahuta <lagutasa@gmail.com>
2024-09-01 23:54:04 +0300

    change KeyManager.java for negative key

--- src/main/java/ua/com/javarush/gnew/crypto/KeyManager.java
+++ src/main/java/ua/com/javarush/gnew/crypto/KeyManager.java
@@ -7,18 +7,15 @@ import ua.com.javarush.gnew.runner.Command;
 public class KeyManager {
    private int key;
     public int key(RunOptions runOptions){
-        if (runOptions.getCommand() == Command.ENCRYPT && runOptions.getKey() >0) {
+        if (runOptions.getCommand() == Command.ENCRYPT && runOptions.getKey() !=0) {
             key = (runOptions.getKey() % 26) * -1;
             return key;
-        }else if (runOptions.getCommand() == Command.DECRYPT && runOptions.getKey() < 0){
+        }else if (runOptions.getCommand() == Command.DECRYPT && runOptions.getKey() != 0) {
             key = (runOptions.getKey() % 26) * -1;
             return key;
-
-        }else if ((runOptions.getKey() % 26) == 0){
+        }else {
             key =7;
             return runOptions.getCommand() == Command.ENCRYPT ? -key : key;
         }
-        key = runOptions.getKey();
-        return key;
     }
 }
