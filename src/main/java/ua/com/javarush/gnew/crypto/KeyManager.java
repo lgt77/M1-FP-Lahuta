ff5bc0fefb8dde57a4666138db370091b067eb5e
Seryii Lahuta <lagutasa@gmail.com>
2024-09-01 22:09:45 +0300

    create class KeyManager.java

---
+++ src/main/java/ua/com/javarush/gnew/crypto/KeyManager.java
@@ -0,0 +1,24 @@
+package ua.com.javarush.gnew.crypto;
+
+import ua.com.javarush.gnew.runner.RunOptions;
+import ua.com.javarush.gnew.runner.Command;
+
+
+public class KeyManager {
+   private Integer key;
+    public Integer key(RunOptions runOptions){
+        if (runOptions.getCommand() == Command.ENCRYPT && runOptions.getKey() >0) {
+            key = (runOptions.getKey() % 26) * -1;
+            return key;
+        }else if (runOptions.getCommand() == Command.DECRYPT && runOptions.getKey() < 0){
+            key = (runOptions.getKey() % 26) * -1;
+            return key;
+
+        }else if ((runOptions.getKey() % 26) == 0){
+            key =7;
+            return runOptions.getCommand() == Command.ENCRYPT ? -key : key;
+        }
+        key = runOptions.getKey();
+        return key;
+    }
+}
