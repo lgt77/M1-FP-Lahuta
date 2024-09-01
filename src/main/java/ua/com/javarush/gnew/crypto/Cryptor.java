5fdc378ed6ebc0167adf5905c17a89d310500deb
Seryii Lahuta <lagutasa@gmail.com>
2024-09-01 22:59:54 +0300

    create class Cryptor.java delete classes Encrypt.java and Decrypt.java

---
+++ src/main/java/ua/com/javarush/gnew/crypto/Cryptor.java
@@ -0,0 +1,24 @@
+package ua.com.javarush.gnew.crypto;
+
+public class Cryptor {
+    public String cypher(String input, int key) {
+
+
+
+        Rotator rotator = new Rotator();
+        char[] charsArray = input.toCharArray();
+
+        StringBuilder sb = new StringBuilder();
+        SymbolsBelonging symBel = new SymbolsBelonging();
+        for(char symbol : charsArray) {
+            if(ConstantsForCryptor.ALPHABET.getCharsArrayConstants().contains(Character.toUpperCase(symbol))){
+                sb.append(symBel.symbolsBelongingABC(symbol, rotator.rotateAlphabet(key)));
+            } else if (ConstantsForCryptor.PUNCTUATION.getCharsArrayConstants().contains(symbol)) {
+                sb.append(symBel.symbolsBelongingPUNCTUATION(symbol, rotator.rotatePunctuation(key)));
+            }else {
+                sb.append(symbol);
+            }
+        }
+        return sb.toString();
+    }
+}
