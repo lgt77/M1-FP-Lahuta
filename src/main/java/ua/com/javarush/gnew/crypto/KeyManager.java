90fb29c80ae820b99e10d91894d2f354b1b38daf
Seryii Lahuta <lagutasa@gmail.com>
2024-09-08 21:30:33 +0300

    added method keySelection(), to add a key when using the bruteforce command

--- src/main/java/ua/com/javarush/gnew/crypto/KeyManager.java
+++ src/main/java/ua/com/javarush/gnew/crypto/KeyManager.java
@@ -1,21 +1,29 @@
 package ua.com.javarush.gnew.crypto;
 
+import ua.com.javarush.gnew.file.FileManager;
 import ua.com.javarush.gnew.runner.RunOptions;
 import ua.com.javarush.gnew.runner.Command;
 
 
 public class KeyManager {
-   private int key;
+
     public int key(RunOptions runOptions){
         if (runOptions.getCommand() == Command.ENCRYPT && runOptions.getKey() !=0) {
-            key = (runOptions.getKey() % 26) * -1;
-            return key;
+            return (runOptions.getKey() % 26) * -1;
         }else if (runOptions.getCommand() == Command.DECRYPT && runOptions.getKey() != 0) {
-            return  (runOptions.getKey() % 26);
-
+            return  runOptions.getKey() % 26;
+        } else if (runOptions.getCommand() == Command.BRUTEFORCE) {
+            return keySelection(runOptions);
         }else {
-            key =7;
-            return runOptions.getCommand() == Command.ENCRYPT ? -key : key;
+            return 0;
         }
     }
+
+    public int keySelection(RunOptions runOptions) {
+        FileManager fileManager = new FileManager();
+        String input =fileManager.read(runOptions.getFilePath());
+        Cryptor cryptor = new Cryptor(input);
+        return ConstantsForCryptor.PUNCTUATION.getCharsArrayConstant().indexOf(cryptor.MostFrequentChar());
+    }
+
 }
