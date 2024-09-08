0c572b18123054395bde8c47f5a2828dd98650ce
Seryii Lahuta <lagutasa@gmail.com>
2024-09-08 21:20:12 +0300

    minor changes for ease of writing code

--- src/main/java/ua/com/javarush/gnew/crypto/ConstantsForCryptor.java
+++ src/main/java/ua/com/javarush/gnew/crypto/ConstantsForCryptor.java
@@ -7,8 +7,8 @@ public enum ConstantsForCryptor {
     ALPHABET(new ArrayList<>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
             'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
             'X', 'Y', 'Z'))),
-    PUNCTUATION (new ArrayList<>(Arrays.asList('.', ',', '!', '?', ':', ';', '-',
-            '(', ')', '[', ']', '{', '}','#', ' ', '@', '*', '+', '=', '_', '&',
+    PUNCTUATION (new ArrayList<>(Arrays.asList(' ', ',', '!', '?', ':', ';', '-',
+            '(', ')', '[', ']', '{', '}','#', '.', '@', '*', '+', '=', '_', '&',
             '\\', '/', '₴', '~', '`')));
 
     private final ArrayList<Character> CHARS_ARRAY_CONSTANT;
